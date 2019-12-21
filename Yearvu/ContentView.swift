8b4bd1dc226214562aa5ac7a6a3571b2cbcb7775
Thiago Ricieri <thiago.ricieri@gmail.com>
2019-12-21 09:22:51 -0300

    Initial Commit

---
+++ Yearvu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yearvu
+//
+//  Created by Thiago Ricieri on 21/12/19.
+//  Copyright © 2019 Thiago Ricieri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
